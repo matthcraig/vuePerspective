939627da16a785793c1054ac5306829e5d35fc40
Matthew Craig <mgcraig78@gmail.com>
2021-08-01 21:33:28 -0700

    added box and transform data and methods

---
+++ app.js
@@ -0,0 +1,35 @@
+const vm = Vue.createApp({
+data(){
+  return{
+    perspective: 100,
+    rotateX: 0,
+    rotateY: 0,
+    rotateZ: 0
+  }
+},
+methods: {
+  reset(){
+      this.perspective = 100
+      this.rotateX = 0
+      this.rotateY = 0
+      this.rotateZ = 0
+  },
+  copy(){
+
+  }
+},
+computed: {
+  box(){
+    return {
+      transform: `
+      perspective(${this.perspective}px)
+      rotateX(${this.rotateX}deg)
+      rotateY(${this.rotateY}deg)
+      rotateZ(${this.rotateZ}deg)
+      `
+    }
+  }
+}
+
+
+}).mount('#app')
